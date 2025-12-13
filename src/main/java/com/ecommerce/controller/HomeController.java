659cc90ce4d31484196370b8ce665d085bbc6ae5
SAKTHIVEL VELMURUGAN <kit28.24bcb044@gmail.com>
2025-12-14 00:11:20 +0530

    minor bug fixes,pom.xml is revised,used java21,created a sample login page in template and created a controller file for it in controller folder

---
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String login() {
+        return "login";  // This refers to home.html inside templates folder
+    }
+}
